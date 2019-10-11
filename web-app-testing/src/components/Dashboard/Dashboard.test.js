a2abc174697801c8c0700e7aded0c8e5377235fa
74ultra <r.barnhill@protonmail.com>
2019-10-10 17:28:09 -0700

    Adding additional tests

--- web-app-testing/src/components/Dashboard/Dashboard.test.js
+++ web-app-testing/src/components/Dashboard/Dashboard.test.js
@@ -8,4 +8,20 @@ describe('<Dashboard />', () => {
         render(<Dashboard />);
     })
 
+    it('Displays all button text', () => {
+        const btnText = render(<Dashboard />);
+        btnText.getByText(/Ball/i)
+        btnText.getByText(/Strike/i)
+        btnText.getByText(/Foul/i)
+        btnText.getByText(/Hit/i)
+        btnText.getByText(/Out/i)
+        btnText.getByText(/Clear all/i)
+    })
+
+    it('Renders all six dashboard buttons', () => {
+        const { getAllByRole } = render(<Dashboard />);
+        const btnArry = getAllByRole('button');
+        expect(btnArry).toHaveLength(6);
+    })
+
 })
